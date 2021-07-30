0b5eb29e011c6b91a18aaad1ccbb2c599be36c76
Vijay Bajadoliya <vijayrd0108@kgmail.com>
2021-07-30 09:05:01 +0530

    react initial commit

---
+++ form/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+
+class App extends Component{
+    render() {
+        return (
+            <div className="App">
+               <Greet /> 
+            </div>
+        );
+    }
+} 
+ 
+export default App;
---
+++ form/src/components/Greet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Greet({ name }) {
+    return <h1> Hello Vijay</h1>
+}
+
+export default Greet
